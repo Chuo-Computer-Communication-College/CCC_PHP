1839d8e523d733acb5d4a5a2ecbaa704ca7bb3d8
ALF <kazuhiro0517150@gmail.com>
2019-07-08 22:40:27 +0900

    Update List2-5

--- cake3app/src/Controller/HelloController.php
+++ cake3app/src/Controller/HelloController.php
@@ -4,13 +4,18 @@ namespace App\Controller;
 class HelloController extends AppController
 {
     public $name = 'Hello';
-    
+
     // public $auto_render = false;
     public $autoRender = false;
 
     public function index()
     {
         echo 'Hello World!';
+    }
+
+    public function other()
+    {
+        echo 'これは、index以外の表示です。';
     }
 }
 
