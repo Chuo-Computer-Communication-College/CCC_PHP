98630aff4b6bc73809ea125e5b95775604a02994
ALF <kazuhiro0517150@gmail.com>
2019-07-08 23:07:06 +0900

    Create List2-7

---
+++ cake3app/src/Controller/HelloController.php
@@ -0,0 +1,29 @@
+<?php
+namespace App\Controller;
+
+class HelloController extends AppController
+{
+    public $name = 'Hello';
+
+    // public $auto_render = false;
+    public $autoRender = false;
+
+    public function index()
+    {
+        // echo 'Hello World!';
+
+        // フォワード
+        // $this->setAction("other");
+        
+        // リダイレクト
+        // $this->redirect("./other");
+        $this->redirect("https://www.yahoo.co.jp/");
+    }
+
+    public function other()
+    {
+        echo 'これは、index以外の表示です。';
+    }
+}
+
+?>
