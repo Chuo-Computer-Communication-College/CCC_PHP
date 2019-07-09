0c0998aa18bf5219d60fc1785b40b0a3a5f27e52
ALF <kazuhiro0517150@gmail.com>
2019-07-10 01:35:09 +0900

    List2-13

--- cake3app/src/Controller/HelloController.php
+++ cake3app/src/Controller/HelloController.php
@@ -3,13 +3,27 @@ namespace App\Controller;
 
 class HelloController extends AppController
 {
-    public $name = 'Hello';
+    public function initialize()
+    {
+        $this->name = 'Hello';
+    
+        $this->autoRender = false;
 
-    public $autoRender = true;
+        $this->viewBuilder()->autoLayout(false);
+    }
 
     public function index()
     {
+        $this->viewBuilder()->autoLayout(true);
         
+        $this->autoRender = true;
+    }
+
+    public function other()
+    {
+        $this->viewBuilder()->autoLayout(false);
+        
+        $this->autoRender = true;
     }
 }
 ?>
---
+++ cake3app/src/Template/Hello/other.ctp
@@ -0,0 +1,17 @@
+<!DocType HTML>
+<html lang="ja">
+  <head>
+    <meta http-equiv="Content-Type" content="Text/HTML"; charset="UTF-8">
+    <title>
+      Hello Page
+    </title>
+  </head>
+  <body>
+    <h1>
+      サンプル見出し
+    </h1>
+    <p>
+      これは、index以外の表示です。
+    </p>
+  </body>
+</html>
