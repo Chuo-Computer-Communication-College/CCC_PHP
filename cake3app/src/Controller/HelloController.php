4a22c4ef0ec0e4d6cf6c6aeb3ced698cfe5fd085
ALF <kazuhiro0517150@gmail.com>
2019-07-09 20:59:38 +0900

    List2-8_9

--- cake3app/src/Controller/HelloController.php
+++ cake3app/src/Controller/HelloController.php
@@ -5,24 +5,11 @@ class HelloController extends AppController
 {
     public $name = 'Hello';
 
-    // public $auto_render = false;
-    public $autoRender = false;
+    public $autoRender = true;
 
     public function index()
     {
-        // echo 'Hello World!';
-
-        // フォワード
-        // $this->setAction("other");
-
-        // リダイレクト
-        // $this->redirect("./other");
-        $this->redirect("https://www.yahoo.co.jp/");
-    }
-
-    public function other()
-    {
-        echo 'これは、index以外の表示です。';
+        $this->viewBuilder()->autoLayout(false);
     }
 }
 ?>
